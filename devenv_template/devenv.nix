f5e7ef8e8e4402028662eb3a9d9969748168dc25
aqu4holic <gautruc.anh1234@gmail.com>
2025-08-03 05:34:01 +0700

    update: newer version, add 2 variables, remove patch

--- devenv_template/devenv.nix
+++ devenv_template/devenv.nix
@@ -1,21 +1,15 @@
 { pkgs, lib, config, inputs, ... }:
 let
-    # Fetch the patch for GPL symbol issues with Linux 6.15
-    gpl_symbols_linux_615_patch = pkgs.fetchpatch {
-        url = "https://github.com/CachyOS/kernel-patches/raw/914aea4298e3744beddad09f3d2773d71839b182/6.15/misc/nvidia/0003-Workaround-nv_vm_flags_-calling-GPL-only-code.patch";
-        hash = "sha256-YOTAvONchPPSVDP9eJ9236pAPtxYK5nAePNtm2dlvb4=";
-        stripLen = 1;
-        extraPrefix = "kernel/";
-    };
+    version = "575.64.05";
+    sha256_64bit = "sha256-hfK1D5EiYcGRegss9+H5dDr/0Aj9wPIJ9NVWP3dNUC0=";
 
     # Define customNvidia using the overridden pkgs
     customNvidia = pkgs.linuxKernel.packages.linux_6_15.nvidia_x11.overrideAttrs (old: {
-        version = "570.153.02";
+        version = version;
         src = pkgs.fetchurl {
-            url = "https://download.nvidia.com/XFree86/Linux-x86_64/570.153.02/NVIDIA-Linux-x86_64-570.153.02.run";
-            sha256 = "148886e4f69576fa8fa67140e6e5dd6e51f90b2ec74a65f1a7a7334dfa5de1b6";
+            url = "https://download.nvidia.com/XFree86/Linux-x86_64/${version}/NVIDIA-Linux-x86_64-${version}.run";
+            sha256 = sha256_64bit;
         };
-        patches = [ gpl_symbols_linux_615_patch ];
     });
 
     buildInputs = with pkgs; [
